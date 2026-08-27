577514ee34e48b1fe73c710cda420daa0d26f82f
Prajina-N <neupaneprajina@gmail.com>
2026-08-26 22:15:38 -0500

    Add script to import paired-end FASTQ sequences

--- paired-end/01_import.sh
+++ paired-end/01_import.sh
@@ -1,1 +1,9 @@
+# Unzip the downloaded sequencing data
+unzip -q YourRawFile.zip
 
+# Import paired-end FASTQ sequences into QIIME 2
+qiime tools import \
+  --type 'SampleData[PairedEndSequencesWithQuality]' \
+  --input-path manifest.txt \
+  --output-path paired-end-demux.qza \
+  --input-format PairedEndFastqManifestPhred33V2
