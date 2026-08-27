11926d4aac71823bd11781ad2437d6f4003c76e8
Prajina-N <neupaneprajina@gmail.com>
2026-08-26 22:23:23 -0500

    Update 01_import.sh with manifest.txt instruction
    Added note about creating manifest.txt before import.

--- paired-end/01_import.sh
+++ paired-end/01_import.sh
@@ -2,6 +2,7 @@
 unzip -q YourRawFile.zip
 
 # Import paired-end FASTQ sequences into QIIME 2
+## Before importing, you need to create manifest.txt file (Refer to QIIME2-workflow_Soil-microbiome-analysis / manifests)
 qiime tools import \
   --type 'SampleData[PairedEndSequencesWithQuality]' \
   --input-path manifest.txt \
